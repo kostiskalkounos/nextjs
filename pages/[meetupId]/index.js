8d514db64fa5cd77d7309f5f9e2a56199de65194
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-20 01:03:17 +0200

    prepare paths with `getStaticPaths` and work with fallback pages

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,6 +11,25 @@ const MeetupDetails = () => {
   );
 };
 
+export const getStaticPaths = async () => {
+  // In dynamic pages, tell nextjs for which dynamic parameter values this page should be pre-generated
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+};
+
 export const getStaticProps = async (context) => {
   // context holds a params object as key
 
@@ -19,13 +38,14 @@ export const getStaticProps = async (context) => {
 
   return {
     props: {
-      meetupData: DUMMY_MEETUPS,
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/a/a7/The_Acropolis_of_Athens_viewed_from_the_Hill_of_the_Muses_%2814220794964%29.jpg",
-      id: meetupId,
-      title: "First Meetup",
-      address: "Some address",
-      description: "This is a first meetup",
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/a/a7/The_Acropolis_of_Athens_viewed_from_the_Hill_of_the_Muses_%2814220794964%29.jpg",
+        id: meetupId,
+        title: "First Meetup",
+        address: "Some address",
+        description: "This is a first meetup",
+      },
     },
     revalidate: 1,
   };
