49a80ef3f282c7fc7d373bd4cb2b0c5117e07d3a
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-19 22:24:34 +0200

    add custom components and css modules

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/a/a7/The_Acropolis_of_Athens_viewed_from_the_Hill_of_the_Muses_%2814220794964%29.jpg"
+      title="First Meetup"
+      address="Some address"
+      description="This is a first meetup"
+    />
+  );
+};
+
+export default MeetupDetails;
