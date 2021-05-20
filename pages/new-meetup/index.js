15b974f8636d91439c052af58bea02b6ad2ae9b4
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-20 02:06:42 +0200

    send HTTP requests to the API Route

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,10 +1,25 @@
 // domain-name.com/new-meetup
 
+import { useRouter } from "next/router";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
-  const addMeetupHandler = (enteredMeetupData) => {
-    console.log(enteredMeetupData);
+  const router = useRouter();
+
+  const addMeetupHandler = async (enteredMeetupData) => {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.replace("/");
   };
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
